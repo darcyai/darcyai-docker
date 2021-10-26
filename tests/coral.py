52f9d21a713352333e9581e1b98d160af1d1776f
Saeid Rezaei Baghbidi <saeid@edgeworx.io>
2021-10-25 17:07:40 -0700

    Updated test scripts

--- tests/coral.py
+++ tests/coral.py
@@ -1,18 +1,22 @@
-from sample_stream import SampleStream
-from darcyai_engine.output.output_stream import OutputStream
 from darcyai_engine.tests.perceptor_mock import PerceptorMock
 from darcyai_engine.pipeline import Pipeline
+from sample_output_stream import SampleOutputStream
+from sample_input_stream import SampleInputStream
 
 def perceptor_input_callback(input_data, pom):
     return input_data
 
 
-input_stream = SampleStream(max_runs=2)
-output_stream = OutputStream()
+def output_stream_callback(pom, input_data):
+    pass
+
+
+input_stream = SampleInputStream(max_runs=2)
+output_stream = SampleOutputStream()
 
 pipeline = Pipeline(input_stream)
 
-pipeline.add_output_stream(output_stream)
+pipeline.add_output_stream("output", output_stream_callback, output_stream)
 
 p1 = PerceptorMock(model_path="models/p1.tflite")
 pipeline.add_perceptor("p1", p1, accelerator_idx=0, input_callback=perceptor_input_callback)
--- tests/sample_input_stream.py
+++ tests/sample_input_stream.py
@@ -3,7 +3,7 @@ from darcyai_engine.input.input_stream import InputStream
 from darcyai_engine.stream_data import StreamData
 from darcyai_engine.utils import timestamp
 
-class SampleStream(InputStream):
+class SampleInputStream(InputStream):
     def __init__(self, max_runs=10):
         self.__stopped = True
         self.__max_runs = max_runs
